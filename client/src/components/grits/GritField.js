3331c505287980a2dc34c556a37857ab0cbb6a52
halkazzar <abylkassym@yandex.com>
2019-02-13 21:35:08 +0000

    rendering simple Form

---
+++ client/src/components/grits/GritField.js
@@ -0,0 +1,15 @@
+// GritField contains logic to render a single label and text input
+
+import React from 'react';
+
+
+export default ({ input}) => { 
+
+   return(
+    <div>
+       <input {...input}/> 
+    </div>
+   );
+};
+
+
--- client/src/components/grits/GritForm.js
+++ client/src/components/grits/GritForm.js
@@ -1,15 +1,32 @@
 // GritForm shows a form for a user to add input
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import GritField from './GritField';
 
 class GritForm extends Component {
-render() {
+
+    renderFields(){
         return (
             <div>
-                Grit Form 
+               <Field type="text" name="task" component={GritField} /> 
             </div>
-        );
+        );    
+        
+    }
+
+
+    render() {
+            return (
+                <div>
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderFields()}
+                    <button type="submit">Submit</button>
+                </form>
+                </div>
+            );
     }
 }
 
-export default GritForm;
+export default reduxForm({
+    form: 'gritForm'    
+})(GritForm);
